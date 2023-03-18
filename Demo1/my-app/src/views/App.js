0aae7e3ecd840e66811340324df16bf441181e95
GemLove <locbkafc123@gmail.com>
2023-03-18 09:39:47 +0700

    update demo1

--- Demo1/my-app/src/views/App.js
+++ Demo1/my-app/src/views/App.js
@@ -5,10 +5,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-text-white">
-          Hello World!!!
-        </p>
+        
+        <div class="container">
+          <div class="col-md-6 col-sm-10 col-lg-5 m-auto">
+            <form class="border p-4 border-info rounded border-2">
+              <div class="mb-3 text-center">
+              
+                <h4 for="exampleInputEmail1" class="form-label ">Form đăng nhập<img src={logo} className="App-logo" alt="logo" /></h4>
+              </div>
+              <div class="mb-3">
+                <label for="txtUsername" class="form-label">Tên đăng nhập</label>
+                <input type="text" class="form-control" id="txtUsername" aria-describedby="txtUsername" />
+              </div>
+              <div class="mb-3">
+                <label for="txtPassword" class="form-label">Mật khẩu</label>
+                <input type="password" class="form-control" id="txtPassword" />
+              </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="chckForget" />
+                <label class="form-check-label" for="chckForget">Ghi nhớ đăng nhập</label>
+              </div>
+              <button type="submit" class="btn btn-primary w-100">Đăng nhập</button>
+            </form>
+          </div>
+        </div>
+
       </header>
     </div>
   );
