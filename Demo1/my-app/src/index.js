ac82ffec2f6483c23d232dc9d3f2d508da8e15e5
GemLove <locbkafc123@gmail.com>
2023-03-19 21:13:13 +0700

    Thử nghiệm thẻ tag

--- Demo1/my-app/src/index.js
+++ Demo1/my-app/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
+class Ok extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <h1>Demo</h1>
+      </div>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+  <App />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- Demo1/my-app/src/views/App.js
+++ Demo1/my-app/src/views/App.js
@@ -1,17 +1,58 @@
 import logo from './logo.svg';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        
-        <div class="container">
+      {/* <div className="container-fluid">
+        <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div> */}
+      <div className="App-header">
+
+        <div className="container">
           <div class="col-md-6 col-sm-10 col-lg-5 m-auto">
             <form class="border p-4 border-info rounded border-2">
               <div class="mb-3 text-center">
-              
-                <h4 for="exampleInputEmail1" class="form-label ">Form đăng nhập<img src={logo} className="App-logo" alt="logo" /></h4>
+
+                <h4 for="exampleInputEmail1" class="form-label h">Đăng nhập<img src={logo} className="App-logo" alt="logo" /></h4>
               </div>
               <div class="mb-3">
                 <label for="txtUsername" class="form-label">Tên đăng nhập</label>
@@ -29,8 +70,7 @@ function App() {
             </form>
           </div>
         </div>
-
-      </header>
+      </div>
     </div>
   );
 }
